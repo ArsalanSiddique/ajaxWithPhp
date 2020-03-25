75e22838824974cc48e870df42326eef01394815
= <arsalan.ahmed1264@gmail.com>
2020-03-25 14:32:59 +0500

    get, post method learned

---
+++ class_02/post.php
@@ -0,0 +1,10 @@
+<?php
+    
+    $name = $_POST["name"];
+    $class = $_POST["class"];
+    $method = $_POST["method"];
+
+    echo "My name is $name and my class is $class";
+    echo "<br />This is $method method.";
+
+?>
